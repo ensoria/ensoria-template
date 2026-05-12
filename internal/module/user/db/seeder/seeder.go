0573f7fd0e7e1686cf7a5e51d6c51bb79a42c81f
ichikawa <68413973+yrichika@users.noreply.github.com>
2026-05-12 17:05:38 +0900

    feat: update user model and seeder implementation

---
+++ internal/module/user/db/seeder/seeder.go
@@ -0,0 +1,27 @@
+package seeder
+
+import (
+	"time"
+
+	encliseeder "github.com/ensoria/encli/pkg/seeder"
+	"github.com/ensoria/gofake/pkg/faker"
+
+	"github.com/ensoria/ensoria-template/internal/module/user/model"
+)
+
+type UserSeeder struct{}
+
+func (s *UserSeeder) TableName() string { return "users" }
+
+func (s *UserSeeder) Seed(f faker.Faker) []model.User {
+	now := time.Now()
+	return []model.User{
+		{Name: f.Person.Name(), CreatedAt: now, UpdatedAt: now},
+		{Name: f.Person.Name(), CreatedAt: now, UpdatedAt: now},
+		{Name: f.Person.Name(), CreatedAt: now, UpdatedAt: now},
+	}
+}
+
+func init() {
+	encliseeder.Add(&UserSeeder{})
+}
--- internal/module/user/model/user_model.go
+++ internal/module/user/model/user_model.go
@@ -6,8 +6,8 @@ import "time"
 // そこまで厳密でないものにしたいため、modelにする
 
 type User struct {
-	Id        int
-	Name      string
-	CreatedAt time.Time // 適当な型
-	UpdatedAt time.Time // 適当な型
+	Id        int       `db:"id"`
+	Name      string    `db:"name"`
+	CreatedAt time.Time `db:"create_at"`  // 適当な型
+	UpdatedAt time.Time `db:"updated_at"` // 適当な型
 }
