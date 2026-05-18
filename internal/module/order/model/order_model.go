9dd69edee828431ce6e45111d561f19d93532dd9
ichikawa <68413973+yrichika@users.noreply.github.com>
2026-05-18 19:40:10 +0900

    fix: remove ID from post seeder entries to ensure default values are applied

--- internal/module/order/model/order_model.go
+++ internal/module/order/model/order_model.go
@@ -7,7 +7,7 @@ type Order struct {
 	UserID        uint          `db:"user_id"`
 	OrderDetailID uint          `db:"order_detail_id"`
 	Total         int           `db:"total"`
-	OrderDetails  []OrderDetail `db:"-"`
+	OrderDetails  []OrderDetail `db:"-"` // dbタグなし、または、"-"、"" の場合はseedから無視される
 	CreatedAt     time.Time     `db:"created_at"`
 	UpdatedAt     time.Time     `db:"updated_at"`
 }
--- internal/module/post/db/seeder/seeder.go
+++ internal/module/post/db/seeder/seeder.go
@@ -18,7 +18,7 @@ func (s *PostSeeder) Seed(f faker.Faker) []model.Post {
 		{UserID: 1, Title: f.Lorem.Sentence(5), Content: f.Lorem.Paragraphs(2, 3), CreatedAt: now, UpdatedAt: now},
 		{UserID: 2, Title: f.Lorem.Sentence(5), Content: f.Lorem.Paragraphs(2, 3), CreatedAt: now, UpdatedAt: now},
 		// NO TITLE: ゼロ値の場合は、カラムに設定された`DEFAULT`が適用される
-		{ID: 20, UserID: 3, Content: f.Lorem.Paragraphs(2, 3), CreatedAt: now, UpdatedAt: now},
+		{UserID: 3, Content: f.Lorem.Paragraphs(2, 3), CreatedAt: now, UpdatedAt: now},
 	}
 }
 
